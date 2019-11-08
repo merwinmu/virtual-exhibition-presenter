25a65f9ae51d09d2a6e5c8f6d0b534fce707de15
Faris  Ahmetasevic <Faris@Faris-MacBook-Air.local>
2019-11-08 12:21:41 +0100

    Joystick script added

--- Assets/Test/PlayerMovement.cs
+++ Assets/Test/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController control;
+    public JoyStickFaris joystick;
 
     public float speed = 5.0f;
     // Start is called before the first frame update
@@ -16,8 +17,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        //float x = Input.GetAxis("Horizontal");
+        //float z = Input.GetAxis("Vertical");
+        float x = joystick.Horizontal();
+        float z = joystick.Vertical();
 
         Vector3 move = transform.right * x + transform.forward * z;
         control.Move(move * speed * Time.deltaTime);
