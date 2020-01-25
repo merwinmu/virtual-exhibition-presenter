f8761ed6213dcd189bf732bbf89952804f901d30
farisahmetasevic <43345602+farisahmetasevic@users.noreply.github.com>
2020-01-25 15:38:01 +0100

    added scroll bar button

---
+++ Assets/ScrollButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollButtonClick : MonoBehaviour
+{
+    private int counter = 0;
+
+    public Button scrollButton;
+    public Sprite firstScroll;
+    public Sprite secondScroll;
+
+    public GameObject scrollBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollBar.gameObject.SetActive(false);
+    }
+    
+    public void OnClickScrollButton()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            scrollBar.gameObject.SetActive(true);
+            scrollButton.image.overrideSprite = secondScroll;
+        }
+
+        if (counter % 2 == 0)
+        {
+            scrollBar.gameObject.SetActive(false);
+            scrollButton.image.overrideSprite = firstScroll;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
